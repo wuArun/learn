9e400706719851c03d3abd02d8a318ec7da6f2b3
wurunxiang <wurunx@gmail.com>
2023-09-05 10:39:57 +0800

    feat: 动态创建类以及运行

--- src/main/java/com/arun/arun_learn/common/DynamicPOJO.java
+++ src/main/java/com/arun/arun_learn/common/DynamicPOJO.java
@@ -1,11 +1,17 @@
 package com.arun.arun_learn.common;
 
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
+import com.google.gson.Gson;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.reflect.Method;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.Collections;
 
 import javax.tools.FileObject;
 import javax.tools.ForwardingJavaFileManager;
@@ -38,28 +44,45 @@ public class DynamicPOJO {
         JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
         StandardJavaFileManager standardFileManager = compiler.getStandardFileManager(null, null, null);
         ClassJavaFileManager classJavaFileManager = new ClassJavaFileManager(standardFileManager);
-        StringObject stringObject = new StringObject(new URI("Student.java"), JavaFileObject.Kind.SOURCE, classString);
-        JavaCompiler.CompilationTask task = compiler.getTask(null, classJavaFileManager, null, null, null,
-                Arrays.asList(stringObject));
-        if (task.call()) {
-            ClassJavaFileObject javaFileObject = classJavaFileManager.getClassJavaFileObject();
-            ClassLoader classLoader = new MyClassLoader(javaFileObject);
-            Object student = classLoader.loadClass("Student").newInstance();
-            //student对象
-            System.out.println("student-->"+student);
-            //获取set方法
-            Method setStudentId = student.getClass().getMethod("setStudentId", int.class);
-            //使用对象赋值
-            Object obj1 = setStudentId.invoke(student, 1);
-            System.out.println("-->setStudentId-->"+setStudentId.toString()+"-->"+obj1);
-            //获取get方法
-            Method getStudentId = student.getClass().getMethod("getStudentId");
-            //使用对象取值
-            Object obj2 = getStudentId.invoke(student);
-            System.out.println("-->getStudentId-->"+getStudentId.toString()+"-->"+obj2);
 
-        }
     }
+
+    private void executeMethod(ClassJavaFileManager classJavaFileManager) throws Exception {
+        ClassJavaFileObject javaFileObject = classJavaFileManager.getClassJavaFileObject();
+        ClassLoader classLoader = new MyClassLoader(javaFileObject);
+        Object student = classLoader.loadClass("Student").newInstance();
+        //student对象
+        System.out.println("student-->" + student);
+        //获取set方法
+        Method setStudentId = student.getClass().getMethod("setStudentId", int.class);
+        //使用对象赋值
+        Object obj1 = setStudentId.invoke(student, 1);
+        System.out.println("-->setStudentId-->" + setStudentId.toString() + "-->" + obj1);
+        //获取get方法
+        Method getStudentId = student.getClass().getMethod("getStudentId");
+        //使用对象取值
+        Object obj2 = getStudentId.invoke(student);
+        System.out.println("-->getStudentId-->" + getStudentId.toString() + "-->" + obj2);
+    }
+
+    /**
+     * 使用自定义类加载器
+     * @param className 类名称
+     * @param classString 类文件详情
+     */
+    private boolean loadClass(String className, String classString,ClassJavaFileManager classJavaFileManager) throws Exception {
+        boolean result = false;
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        try {
+            StringObject stringObject = new StringObject(new URI(className + JavaFileObject.Kind.SOURCE), JavaFileObject.Kind.SOURCE, classString);
+            result = compiler.getTask(null, classJavaFileManager, null, null, null,
+                    Collections.singletonList(stringObject)).call();
+        } catch (Exception e) {
+            System.out.println("执行错误");
+        }
+        return result;
+    }
+
 
     /**    *自定义fileManager    */
     static class ClassJavaFileManager extends ForwardingJavaFileManager{
@@ -123,8 +146,12 @@ public class DynamicPOJO {
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        createStudent();
-    }
 
+    public static void main(String[] args) {
+        String s = "在不？魔域2礼包领了吗，没领加\\/ ：  \"Jokes1990\"";
+        Gson gson = new Gson();
+        String replace = "{\"send_time\":1682330537,\"server_id\":1087,\"chat_channel\":\"2\",\"source_id\":286143606286222,\"source_name\":\"多拉哈蔓\",\"source_vip\":0,\"target_id\":0,\"target_name\":\"某某某某\",\"message\":\"在不？魔域2礼包领了吗，没领加\\/ ：  Jokes1990\",\"imei\":\"4e562f8ac848c1461795d0e0afc24b64\",\"plat\":\"android\",\"version\":\"1.0.133.19823\",\"remark\":\"0\",\"checkcode\":\"48b9f0fc83d5b8778ac8880b6dd2a3f1\"}".replace("\\", "\\\\");
+        ClientChatEntity clientChatEntity = gson.fromJson(replace, ClientChatEntity.class);
+        System.out.println(clientChatEntity.getMessage());
+    }
 }
--- src/main/java/com/arun/arun_learn/util/JdbcUtil.java
+++ src/main/java/com/arun/arun_learn/util/JdbcUtil.java
@@ -1,7 +1,6 @@
 package com.arun.arun_learn.util;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson2.JSON;
 import com.arun.arun_learn.common.Constants;
 import com.arun.arun_learn.connection.IConnection;
 import com.arun.arun_learn.entity.EquipmentInfo;
